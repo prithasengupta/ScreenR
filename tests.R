18841eddd20c1add2dfe43d37c1c91f87b605754
Pritha Sengupta <psengupta@wesleyan.edu>
2023-11-16 15:50:57 -0500

    Chi-sqaure

--- tests.R
+++ tests.R
@@ -25,3 +25,13 @@ mtcars$cyl=as.character(mtcars$cyl)
 x=cp_test(mtcars, 'mpg', 'cyl')
 print(x)
 
+#categorical vs categorical
+
+chitest <- function(data, dep, indep, sig=0.05)
+{
+  data[dep] <- as.factor(data[dep])
+  data[indep] <- as.factor(data[indep])
+  chistat <- chisq.test(data[dep], data[indep], correct=FALSE)
+  return(chistat$p.value)
+}
+
